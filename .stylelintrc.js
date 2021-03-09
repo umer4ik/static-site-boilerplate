b0d31f07f1a79d20d706fdb161a7e6b882a208fd
Umer <umer.hurramov@gmail.com>
2021-03-09 21:02:05 +0200

    Update .stylelintrc.js

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -4,6 +4,13 @@ module.exports = {
   "rules": {
     "at-rule-no-unknown": null,
     "scss/at-rule-no-unknown": true,
-    "no-descending-specificity": null
+    "no-descending-specificity": null,
+    "rule-empty-line-before": null,
+    "declaration-empty-line-before": null,
+    "block-no-empty": null,
+    "no-missing-end-of-source-newline": null,
+    "number-leading-zero": null,
+    "at-rule-empty-line-before": null,
+    "color-hex-case": null,
   },
 }
